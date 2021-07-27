4e5189ab6d1557e2fe977a5a74a8ac7c2f82aaf5
tejas kashyap <tejasisboss@gmail.com>
2021-07-27 20:05:18 +0530

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basics/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //Data Types
+    /*bool isMale = true;
+
+    var day = "hello";
+    const ha = 4;*/
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome..!"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
